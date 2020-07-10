b8b142b4f5d7747bbdfa2fa5f48cf298adcf370a
Matthew Nienow <rockopera8@gmail.com>
2020-07-10 08:20:57 -0500

    Added char code description for locale table

--- src/utils/locale/locale-list/LocaleList.tsx
+++ src/utils/locale/locale-list/LocaleList.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { COMMON_LOCALES } from '../locales';
 
+const charCodes: { [key: number]: string } = {
+  44: 'Comma',
+  46: 'Period',
+  160: 'No-Break Space',
+  1643: 'Arabic Decimal Separator',
+  1644: 'Arabic Thousands Separator',
+  8217: 'Right Single Quotation Mark',
+  8239: 'Narrow No-Break Space',
+  0: 'None'
+};
+
 function LocaleList() {
 
   function renderRow(locale: string) {
@@ -9,10 +20,11 @@ function LocaleList() {
     try {
       const result = new Intl.NumberFormat(locale).format(1234.56);
       if (result.length === 8) {
-        thousand = `${result[1]} (${result.charCodeAt(1)})`;
-        decimal = `${result[5]} (${result.charCodeAt(5)})`;
+        thousand = `${result[1]} (${charCodes[result.charCodeAt(1)]})`;
+        decimal = `${result[5]} (${charCodes[result.charCodeAt(5)]})`;
       } else if (result.length === 7) {
-        decimal = `${result[4]} (${result.charCodeAt(4)})`;
+        thousand = `(${charCodes[0]})`;
+        decimal = `${result[4]} (${charCodes[result.charCodeAt(4)]})`;
       }
     } catch {
     }
